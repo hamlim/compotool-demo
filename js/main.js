c1dc322a68f3dd85bdee387e4d533ee123fa32eb
hamlim <hamlim@rpi.edu>
2016-02-18 12:57:31 -0500

    adding some content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+$(document).ready(function() {
+    //we need to calc the formula
+    var button = document.getElementsByClassName('form__submit');
+    var input = document.getElementsByClassName('form__input');
+    var select = document.getElementsByClassName('form__select')[0];
 
+    button.onclick = function() {
+        //lets determine the val
+        if (input.value === ""){
+            alert("Please enter a value in the first form field!");
+        } else {
+            var val = input.value;
+            var selectval = select.value;
+        }
+    }
+
+});
